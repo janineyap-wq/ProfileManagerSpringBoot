52799856940ee0be38459467bd39007acc422cf6
Ant6769 <anthony_aquino@dlsu.edu.ph>
2026-07-23 18:53:42 +0800

    Connect Spring Boot to Supabase PostgreSQL session pooler and setup avatar storage.

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanager/profilemanager/ProfileManagerSpringBootApplication.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanager/profilemanager/ProfileManagerSpringBootApplication.java
@@ -1,4 +1,4 @@
-package ph.edu.dlsu.lbycpob.profilemanager;
+package ph.edu.dlsu.lbycpob.profilemanager.profilemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
