c71d909e48dd7f7a1a2bf2be4c5d03edf85a57d6
cassandramazo-sys <cassandra_mazo@dlsu.edu.ph>
2026-07-23 16:15:16 +0800

    edited the package as it was in the wrong naming coonvention

--- src/test/java/ph/edu/dlsu/lbycpob/profilemanager/profilemanager/ProfileManagerSpringBootApplicationTests.java
+++ src/test/java/ph/edu/dlsu/lbycpob/profilemanager/profilemanager/ProfileManagerSpringBootApplicationTests.java
@@ -1,4 +1,4 @@
-package ph.edu.dlsu.lbycpob.profilemanager;
+package ph.edu.dlsu.lbycpob.profilemanager.profilemanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
