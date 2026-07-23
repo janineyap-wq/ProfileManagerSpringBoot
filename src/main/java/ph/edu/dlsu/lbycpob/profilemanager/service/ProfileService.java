e13a7c68755fba402df49ba349ff6e0524928bdd
Janeah Laureen <janine_yap@dlsu.edu.ph>
2026-07-23 16:31:57 +0800

    Edited the Profile Service

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanager/service/ProfileService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanager/service/ProfileService.java
@@ -14,3 +14,25 @@ import java.util.UUID;
 
 @Service
 public class ProfileService {
+    private final ProfileRepository profileRepository;
+    private final FriendRepository friendRepository;
+    private final ImageCompressionService imageCompressionService;
+    private final SupabaseStorageService supabaseStorageService;
+
+    public ProfileService(ProfileRepository profileRepository, FriendRepository friendRepository, ImageCompressionService imageCompressionService, SupabaseStorageService supabaseStorageService) {
+        this.profileRepository = profileRepository;
+        this.friendRepository = friendRepository;
+        this.imageCompressionService = imageCompressionService;
+        this.supabaseStorageService = supabaseStorageService;
+    }
+
+    public List<Profile> listProfiles() {
+        return profileRepository.findAllByOrderByNameAsc();
+    }
+
+    public Profile getProfile(UUID id) {
+        return profileRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Profile not found."));
+    }
+
+}
