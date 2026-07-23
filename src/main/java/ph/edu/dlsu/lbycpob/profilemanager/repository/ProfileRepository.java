a8b74efe129f024355e958ef6a26f85c252d3da5
Janeah Laureen <janine_yap@dlsu.edu.ph>
2026-07-23 16:21:11 +0800

    Added ProfileRepository.java packages

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanager/repository/ProfileRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanager/repository/ProfileRepository.java
@@ -1,4 +1,8 @@
 package ph.edu.dlsu.lbycpob.profilemanager.repository;
 
-public interface ProfileRepository {
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import ph.edu.dlsu.lbycpob.profilemanager.model.Profile;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
