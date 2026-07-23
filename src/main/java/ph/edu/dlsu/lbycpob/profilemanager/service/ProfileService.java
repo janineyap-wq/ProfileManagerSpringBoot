c7ca0ced03734ba46c86f21dc51b508251adbd0f
Janeah Laureen <janine_yap@dlsu.edu.ph>
2026-07-23 16:31:14 +0800

    Added ProfileService. java

--- src/main/java/ph/edu/dlsu/lbycpob/profilemanager/service/ProfileService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/profilemanager/service/ProfileService.java
@@ -1,4 +1,16 @@
 package ph.edu.dlsu.lbycpob.profilemanager.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import ph.edu.dlsu.lbycpob.profilemanager.model.Friend;
+import ph.edu.dlsu.lbycpob.profilemanager.model.Profile;
+import ph.edu.dlsu.lbycpob.profilemanager.repository.FriendRepository;
+import ph.edu.dlsu.lbycpob.profilemanager.repository.ProfileRepository;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+@Service
 public class ProfileService {
-}
