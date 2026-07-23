db1ac501675034aee42ab83d7539d6244c6cbafa
Janeah Laureen <janine_yap@dlsu.edu.ph>
2026-07-23 14:55:26 +0800

    Initial commit

---
+++ src/main/java/org/example/profilemanagerspringboot/ProfileManagerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.profilemanagerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileManagerSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileManagerSpringBootApplication.class, args);
+    }
+
+}
